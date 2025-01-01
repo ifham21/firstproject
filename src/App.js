6c767e765e53349cf0655d84f53b24ee9a4fb912
Ahmed Ifham <ifham.maa@gmail.com>
2025-01-01 13:27:17 +0530

    updated the AddItems functionality

--- src/App.js
+++ src/App.js
@@ -26,6 +26,20 @@ function App() {
     ]
   )
 
+  // create a state for adding items
+  const [newItem, setNewItem] = useState('')
+
+  //adding the variable created via the state, to the items array
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const addNewItem = {id, checked: false, item}
+    const listItems = [...items, addNewItem]
+    //set the new item to the state of lists
+    setItems(listItems)
+    // Storing the data into a local storage
+    localStorage.setItem("todo_list", JSON.stringify(listItems))
+  }
+
   const handleCheck = (id) => {
     const listItems = items.map((item) => 
       item.id === id ? {...item, checked: !item.checked} 
@@ -46,11 +60,27 @@ function App() {
   }
 
 
+  //submit event for adding new items
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!newItem) return;
+    console.log(newItem)
+
+    // addItem to the list
+    addItem(newItem)
+    // to make the field empty
+    setNewItem('')
+  }
+
   return (
     <div className="App">
       {/* Sending tite using props */}
         <Header title = "My Todo"/>
-        <AddItem />
+        <AddItem 
+          newItem = {newItem}
+          setNewItem = {setNewItem}
+          handleSubmit = {handleSubmit}
+        /> 
         <Content 
           items = {items}
           handleCheck = {handleCheck}
