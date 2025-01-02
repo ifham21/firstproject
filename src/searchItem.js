fdab57cde689f4a842c18d9f98f2084f97518455
Ahmed Ifham <ifham.maa@gmail.com>
2025-01-02 23:18:57 +0530

    Creating Search Items

--- src/searchItem.js
+++ src/searchItem.js
@@ -3,7 +3,13 @@ import React from 'react'
 const searchItem = () => {
   return (
     <form className='searchForm' onSubmit={(e) => e.preventDefault}>
-
+      <label htmlFor="search">Search</label>
+      <input 
+        type="text" 
+        id="search" 
+        role='searchbox' 
+        placeholder='Seach Items' 
+      />
 
     </form>
   )
